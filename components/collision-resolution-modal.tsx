e7728adddd1c9cda43937f0916d38ca145661fae
ragul26061999 <ragul26061999@gmail.com>
2026-06-11 16:37:34 +0530

    refactor: update relative import path for Task type in collision-resolution-modal

--- components/collision-resolution-modal.tsx
+++ components/collision-resolution-modal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Task } from '@/lib/scheduling-collision';
+import { Task } from '../lib/scheduling-collision';
 
 interface CollisionResolutionModalProps {
   isOpen: boolean;
